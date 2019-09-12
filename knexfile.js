d9eb8209abbe43e00f829da453f76193680279c7
Alan Lemasney <alanlemasney34@yahoo.com>
2019-09-11 20:27:39 -0600

    removed extra bracket in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -8,13 +8,14 @@ module.exports = {
       filename: './data/data.db3'
     },
     useNullAsDefault: true,
- 
+  },
 
   pool: {
     afterCreate: (conn, done) => {
       conn.run('PRAGMA foreign_keys = ON', done);
     },
   },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
@@ -23,9 +24,9 @@ module.exports = {
       directory: './database/migrations',
       tableName: 'dbmigrations',
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { 
+      directory: './database/seeds' 
+    },
   }
-  
-},
 
 };
